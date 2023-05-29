49110e56fd78d2163d611ef76a0fa46e99e506e6
papyntown <realistedem@gmail.com>
2023-05-29 19:18:22 +0200

    continue

--- src/components/MapResium.tsx
+++ src/components/MapResium.tsx
@@ -1,15 +1,56 @@
-import { Viewer } from "resium";
-import { Ion, createWorldTerrain } from "cesium";
+import { CameraFlyTo, Entity, PointGraphics, Viewer } from "resium";
+import { Ion, createWorldTerrain, Cartesian3, JulianDate } from "cesium";
+import data from "./data/data.json";
 
 const MapResium = () => {
     // Le token Cesium pour avoir accès a la planete etc ...
     Ion.defaultAccessToken =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmZDE1MWZkNy03ODA2LTQ1ZGUtYTU0Ny0wODk4YjJmNjRlNjgiLCJpZCI6MTM2MDM2LCJpYXQiOjE2ODI2Nzg0MTJ9.iHoaBQzYta71j7qcvnIAMkqQBowphzGGGnntMlqiCwI";
     const worldTerrain = createWorldTerrain();
+    const dataPoint = {
+        longitude: -122.38985,
+        latitude: 37.61864,
+        height: -27.32,
+    };
+
+    const flightData = data.map((item, index) => (
+        <Entity
+            key={index}
+            position={Cartesian3.fromDegrees(
+                item.longitude,
+                item.latitude,
+                item.height
+            )}>
+            {" "}
+            <PointGraphics pixelSize={10} />{" "}
+        </Entity>
+    ));
+    /* Initialiser l'horloge du visualiseur : */
+    const timeStepInSeconds = 30; // Intervalle de temps en secondes entre chaque étape
+    const totalSeconds = timeStepInSeconds * (flightData.length - 1); // Calcul du nombre total de secondes
+    const start = JulianDate.fromIso8601("2020-03-09T23:10:00Z"); // Définition de la date de début
+    const stop = JulianDate.addSeconds(start, totalSeconds, new JulianDate()); // Calcul de la date de fin en ajoutant les secondes au début
+
     return (
         <div>
             <Viewer terrainProvider={worldTerrain} full>
-                {/* <Entity position={position} point={pointGraphics} /> */}
+                <Entity
+                    position={Cartesian3.fromDegrees(
+                        dataPoint.longitude,
+                        dataPoint.latitude,
+                        dataPoint.height
+                    )}>
+                    <PointGraphics pixelSize={10} />
+                    <CameraFlyTo
+                        duration={5}
+                        destination={Cartesian3.fromDegrees(
+                            dataPoint.longitude,
+                            dataPoint.latitude,
+                            dataPoint.height + 1500
+                        )}
+                    />
+                    {flightData}
+                </Entity>
             </Viewer>
         </div>
     );
